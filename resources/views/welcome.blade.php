a76d86b0684a8866ec578273d69be9bf31e82bc5
Muqiit <mmuqiit.f14@gmail.com>
2021-07-05 14:06:12 +0700

    edit-home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HMIF ITENAS
                 </div>
 
                 <div class="links">
