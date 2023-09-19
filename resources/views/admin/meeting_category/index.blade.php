c0155ffe70fdd6bea0bf0fa64700ee65b935d5be
MartinZakhaev <andikawahyusyaputra@gmail.com>
2023-09-19 14:44:13 +0700

    minor bug fix

--- resources/views/admin/meeting_category/index.blade.php
+++ resources/views/admin/meeting_category/index.blade.php
@@ -174,7 +174,7 @@
                         </div>
                         <a href="{{ route('admin.meeting_category.create') }}" class="btn btn-primary">
                             <i class="far fa-plus-circle"></i>
-                            Buat Kategori Rapat
+                            Buat Kategori Rapatttt
                         </a>
                     </div>
                     <div class="card-body">
