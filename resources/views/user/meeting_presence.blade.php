a4039ea68cc870e0648b91497cd811de3e2c4422
MartinZakhaev <andikawahyusyaputra@gmail.com>
2023-10-07 09:48:38 +0700

    [BUG] minor bug fixing, typo fixed

--- resources/views/user/meeting_presence.blade.php
+++ resources/views/user/meeting_presence.blade.php
@@ -151,7 +151,7 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header d-flex">
-                            Informasi dan Presensi Rapat Terkini
+                            Informasi dan Presensi Rapat Terkiniiiiii
                         </div>
                         <div class="card-body">
                             <div id="reader" width="600px"></div>
